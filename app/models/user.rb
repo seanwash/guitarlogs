d7c6ed28b0d8b6ecce269fbf382d84adf339e08c
Sean Washington <hello@seanwash.com>
2015-05-02 16:00:09 -0700

    Some really gross formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+
   has_one :journal
 end

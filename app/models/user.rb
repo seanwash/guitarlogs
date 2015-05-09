2c39ccdb5c6377864f162982eca86b66350eecd7
Sean Washington <hello@seanwash.com>
2015-05-09 15:14:13 -0700

    Force emails to be saved as downcased

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,15 @@ class User < ActiveRecord::Base
 
   has_many :exercises
 
+  # Make sure that emails are stored lowercase
+  before_save :downcase_emails
+
   # Always make sure that a user has a journal
   after_create :create_journal
+
+  def downcase_emails
+    self.email = email.downcase
+  end
 
   def create_journal
     Journal.create user: self
